78fa318467adbfff327400161bdc63c138a3c98d
BILLY K. <billykibet.dev@gmail.com>
2024-01-19 16:59:30 +0300

    Done Project View

--- src/App/pages/ProjectView/ProjectView.jsx
+++ src/App/pages/ProjectView/ProjectView.jsx
@@ -10,31 +10,35 @@ function ProjectView() {
       <div className="p-image-holder">
         <img src={tempImg} alt="" />
         <div className="p-art-holder">
-          <button>View Code</button>
-          <button>Share Link</button>
+          <div className="p-art-buttons">
+            <button>View Code</button>
+            <button>Share Link</button>
+          </div>
         </div>
       </div>
 
       <h2 className="title">Authentication App</h2>
       {/*  */}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-        quaerat architecto dolorum, delectus ex esse nisi, molestias eveniet,
-        laborum temporibus obcaecati enim officiis sapiente! Aspernatur laborum
-        deleniti ab nihil quibusdam.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vitae,
-        iure aliquid illum quaerat error delectus voluptate iusto sequi ducimus
-        repellat. Eaque officia beatae tenetur illum aperiam eligendi nisi sit.
-      </p>
-      {/*  */}
+      <div className="p-p">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          quaerat architecto dolorum, delectus ex esse nisi, molestias eveniet,
+          laborum temporibus obcaecati enim officiis sapiente! Aspernatur
+          laborum deleniti ab nihil quibusdam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vitae,
+          iure aliquid illum quaerat error delectus voluptate iusto sequi
+          ducimus repellat. Eaque officia beatae tenetur illum aperiam eligendi
+          nisi sit.
+        </p>
+        {/*  */}
+      </div>
 
       <p className="p-langs">React, Javascript, Spring</p>
-
       <div className="p-navigation">
         <button>Previous Project</button>
-        <button>All Projects</button>
+        <button className="">All Projects</button>
         <button>Next Project</button>
       </div>
     </div>
